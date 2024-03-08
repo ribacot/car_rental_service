6f64200db55adc7e7a191c9ab44888835cc5846c
Andrii_Kuzhelnii <remontdiskov1979@gmail.com>
2024-03-08 14:12:04 +0200

    Update ModalWrapper.jsx

--- src/components/Modal/ModalWrapper.jsx
+++ src/components/Modal/ModalWrapper.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from "react";
+// import { useCallback, useEffect } from "react";
 import BtnClose from "../Ui/BtnClose";
 import useClose from "../../utilites/useClose";
 
